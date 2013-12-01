c50c3b45d3674a998aab1ceaea26b65f0d194c68
Lars Kanis <lars@greiz-reinsdorf.de>
2013-12-01 21:38:23 +0100

    Fix documentation of Windows related versions

--- lib/shellwords/cmd_exe.rb
+++ lib/shellwords/cmd_exe.rb
@@ -1,14 +1,9 @@
 module Shellwords::CmdExe
-  # Splits a string into an array of tokens in the same way the UNIX
-  # Bourne shell does.
+  # Splits a string into an array of tokens in the same way the Windows
+  # cmd.exe shell does for %1 to %9 parameters.
   #
   #   argv = Shellwords.split('here are "two words"')
-  #   argv #=> ["here", "are", "two words"]
-  #
-  # String#shellsplit is a shortcut for this function.
-  #
-  #   argv = 'here are "two words"'.shellsplit
-  #   argv #=> ["here", "are", "two words"]
+  #   argv #=> ["here", "are", "\"two words\""]
   def self.split(line)
     words = []
     field = ''
@@ -24,36 +19,6 @@ module Shellwords::CmdExe
     words
   end
 
-  # Escapes a string so that it can be safely used in a Bourne shell
-  # command line.  +str+ can be a non-string object that responds to
-  # +to_s+.
-  #
-  # Note that a resulted string should be used unquoted and is not
-  # intended for use in double quotes nor in single quotes.
-  #
-  #   argv = Shellwords.escape("It's better to give than to receive")
-  #   argv #=> "It\\'s\\ better\\ to\\ give\\ than\\ to\\ receive"
-  #
-  # String#shellescape is a shorthand for this function.
-  #
-  #   argv = "It's better to give than to receive".shellescape
-  #   argv #=> "It\\'s\\ better\\ to\\ give\\ than\\ to\\ receive"
-  #
-  #   # Search files in lib for method definitions
-  #   pattern = "^[ \t]*def "
-  #   open("| grep -Ern #{pattern.shellescape} lib") { |grep|
-  #     grep.each_line { |line|
-  #       file, lineno, matched_line = line.split(':', 3)
-  #       # ...
-  #     }
-  #   }
-  #
-  # It is the caller's responsibility to encode the string in the right
-  # encoding for the shell environment where this string is used.
-  #
-  # Multibyte characters are treated as multibyte characters, not bytes.
-  #
-  # Returns an empty quoted String if +str+ has a length of zero.
   def self.escape(str)
     raise NotImplemented
   end
--- lib/shellwords/msvcrt_by_cmd_exe.rb
+++ lib/shellwords/msvcrt_by_cmd_exe.rb
@@ -1,13 +1,8 @@
 module Shellwords::MsvcrtByCmdExe
-  # Splits a string into an array of tokens in the same way the UNIX
-  # Bourne shell does.
+  # Splits a string into an array of tokens in the same way the Microsoft
+  # C-runtime does when started by the cmd.exe shell.
   #
   #   argv = Shellwords.split('here are "two words"')
-  #   argv #=> ["here", "are", "two words"]
-  #
-  # String#shellsplit is a shortcut for this function.
-  #
-  #   argv = 'here are "two words"'.shellsplit
   #   argv #=> ["here", "are", "two words"]
   def self.split(line)
     cmdline = ''
@@ -19,34 +14,15 @@ module Shellwords::MsvcrtByCmdExe
     Shellwords::Msvcrt.split(cmdline)
   end
 
-  # Escapes a string so that it can be safely used in a Bourne shell
-  # command line.  +str+ can be a non-string object that responds to
-  # +to_s+.
+  # Escapes a string so that it can be safely used in a Windows cmd.exe
+  # command line for most C applications.
+  # +str+ can be a non-string object that responds to +to_s+.
   #
   # Note that a resulted string should be used unquoted and is not
   # intended for use in double quotes nor in single quotes.
   #
   #   argv = Shellwords.escape("It's better to give than to receive")
-  #   argv #=> "It\\'s\\ better\\ to\\ give\\ than\\ to\\ receive"
-  #
-  # String#shellescape is a shorthand for this function.
-  #
-  #   argv = "It's better to give than to receive".shellescape
-  #   argv #=> "It\\'s\\ better\\ to\\ give\\ than\\ to\\ receive"
-  #
-  #   # Search files in lib for method definitions
-  #   pattern = "^[ \t]*def "
-  #   open("| grep -Ern #{pattern.shellescape} lib") { |grep|
-  #     grep.each_line { |line|
-  #       file, lineno, matched_line = line.split(':', 3)
-  #       # ...
-  #     }
-  #   }
-  #
-  # It is the caller's responsibility to encode the string in the right
-  # encoding for the shell environment where this string is used.
-  #
-  # Multibyte characters are treated as multibyte characters, not bytes.
+  #   argv #=> "^\"It's better to give than to receive^\""
   #
   # Returns an empty quoted String if +str+ has a length of zero.
   def self.escape(str)
